eb47324e8ce8a0e1bafe8ea4de68e82663ff285c
SoulamaH <hassanesoulama@gmail.com>
2018-05-14 20:01:52 +0000

    creation d'une classe concrete

--- src/Lanceur.java
+++ src/Lanceur.java
@@ -4,5 +4,10 @@ public class Lanceur {
 public static void main(String[] args) {
 	
 	System.out.println("Ma premier demo git");
+	
+	System.out.println("Demo sur les branches");
+	
 	}
+	
+	
 }
---
+++ src/NewClasse.java
@@ -0,0 +1,11 @@
+import GitInt.MonInterface;
+
+public class NewClasse implements MonInterface {
+
+	@Override
+	public void faireLaSomme(int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
